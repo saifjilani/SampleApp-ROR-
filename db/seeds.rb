495d28e7f867b36fb39d89e67ce9721bdfde3254
Saif Jilani <saifjilani@hotmail.com>
2016-05-12 00:37:13 -0400

    Add password resets & email configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Saif Jilani",
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
+5.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
