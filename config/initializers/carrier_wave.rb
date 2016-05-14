05f32af675e1db11e271b0a0b2845b544822e82c
Saif Jilani <saifjilani@hotmail.com>
2016-05-14 00:01:33 -0400

    modified fog credentials to path = true

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,7 @@ if Rails.env.production?
       # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'], 
-      :path_style => true
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
