d2a53805ddca1596132d80adbdca3038b18304a6
Saif Jilani <saifjilani@hotmail.com>
2016-05-14 23:53:14 -0400

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Users
 User.create!(name:  "Saif Jilani",
              email: "saifjilani93@gmail.com",
              password:              "foobar",
@@ -18,8 +19,18 @@ User.create!(name:  "Saif Jilani",
               activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..5]
+followers = users[3..4]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
